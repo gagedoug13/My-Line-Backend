5b801f3e1ba2689d310ebec49babf485f3a9e581
Gage Thornberry <gagedoug13@gmail.com>
2019-12-14 17:39:26 -0700

    cleaned up code quite a bit.

--- seeds/users.js
+++ seeds/users.js
@@ -1,14 +1,12 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    // .then(function () {
-    //   // Inserts seed entries
-    //   return knex('users').insert([
-    //     { username: 'cody',
-    //     email: 'cody@cody.cody'
-    //     }
 
-    //   ]);
-    // });
+  return knex('users').del()
+    .then(function () {
+      return knex('users').insert([
+        { username: 'gage',
+        email: 'gage@gage.com'
+        }
+      ]);
+    });
 };
